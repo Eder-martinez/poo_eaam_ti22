57671d4d1bc86e006eeb5f5f62f54274d150f792
Eder-martinez <122801090+Eder-martinez@users.noreply.github.com>
2023-01-23 14:51:56 +0000

    comentarios multilínea, unilineal y concatenación

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 |No.|Arichivo|Descripcion|
 |--|--|--|
 |1|programa0.py|Hola mundo en Python3|
+|2|programa1.py|comentarios multilinea unilinea y concatenacion|
+
---
+++ semana2/programa1.py
@@ -0,0 +1,9 @@
+"""
+    programa1.py
+    nombre:Eder AAM
+    fecha:23/01/2023
+    descripcion:funcion print, comentarios multimedia
+"""
+variable1 = "hola "  #  variable para almacenar una cadena de caracteres
+variable2 = "mundo python"  #  variable para almacenar una cadena de caracteres
+print(variable1+variable2)
