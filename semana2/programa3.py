5e55676f2848ce313d3529b2588bbf472abddcc5
Eder-martinez <122801090+Eder-martinez@users.noreply.github.com>
2023-01-24 14:58:05 +0000

    se aprendió a sumar, multiplicar y dividir, la multiplicación esta inconclusa

---
+++ semana2/programa3.py
@@ -0,0 +1,12 @@
+"""
+    programa2.py
+    nombre:Eder AAM
+    fecha:24/01/2023
+    descripcion: se aprendio a sumar, multiplicar y dividir
+"""
+variable1 = 10  #  variable para almacenar una cadena de caracteres
+variable2 = 5   #  variable para almacenar una cadena de caracteres
+print(variable1 + variable2)  #  funcion para sumar o convinar variables
+print("{} + {} = {}" .format(variable1, variable2,(variable1 + variable2)))  #  funncion para sumar variables
+print("{} * {} = {}" .format(variable1, variable2,(variable1 * variable2)))  #  funncion para multiplicar variables
+print("el resultado de dividir: {} / {} = {}" .format(variable1, variable2,(variable1 / variable2)))  #  funncion para dividir variables
